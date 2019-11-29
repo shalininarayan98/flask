3dda2f57aca4176404dd396a922aa10924c5807e
sn.shalini1@hotmail.co.uk <sn.shalini1@hotmail.co.uk>
2019-11-29 15:28:25 +0000

    third commit

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,29 @@
-from flask import render_template
-from application import app
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
+
+
+
+dummyData = [
+        {
+            "name": {"first":"Chester", "last":"Gardner"},
+            "title":"First Post",
+            "content":"This is some dummy data for Flask lectures"
+        },
+        {
+            "name": {"first":"Chris", "last":"Perrins"},
+            "title":"Second Post",
+            "content":"This is even more dummy data for Flask lectures"
+        }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title = 'Home')
+    postData = Posts.query.all()
+    return render_template('home.html', title = 'Home', posts=postData)
 
 
 @app.route('/about')
@@ -21,3 +39,22 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html', title = 'Register')
+
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            title=form.title.data,
+            content=form.content.data
+    )
+        db.session.add(postData)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        print(form.errors)
+        return render_template('post.html',title='Post', form=form)
